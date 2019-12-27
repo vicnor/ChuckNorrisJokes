9503a1694f6f6d26b730e99b6a9cab7e0bdc5b4f
Victor Nordstrand <v_nordstrand@outlook.com>
2019-12-27 12:21:55 +0100

    first commit

---
+++ JokeApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JokeApp.Models;
+using JokeApp.Services;
+
+namespace JokeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IJokeService jokeService;
+
+        public HomeController()
+        {
+            this.jokeService = new JokeService();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Joke"] = jokeService.GetJoke();
+            return View();
+        }
+
+    }
+}
